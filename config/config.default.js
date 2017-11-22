dfb8476f76ef0790376afd21151663e2cd581e08
okoala <dapixp@gmail.com>
2017-11-22 18:01:04 +0800

    feat: postcss use callback to load config

--- config/config.default.js
+++ config/config.default.js
@@ -1,15 +1,16 @@
-exports.mobileFlexible = {};
+exports.mobileFlexible = {
+  baseDpr: 2, // base device pixel ratio (default: 2)
+  remUnit: 75, // rem unit value (default: 75)
+  remPrecision: 6, // rem value precision (default: 6)
+  hairlineClass: 'hairlines', // class name of 1px border (default: 'hairlines')
+  autoRem: true, // whether to transform to rem unit (default: false)
+};
 
 exports.styledJsx = {
   postcss: {
     plugins: {
-      'postcss-adaptive': require('postcss-adaptive')({
-        // baseDpr: 2,                 // base device pixel ratio (default: 2)
-        // remUnit: 75,                // rem unit value (default: 75)
-        // remPrecision: 6,            // rem value precision (default: 6)
-        // hairlineClass: 'hairlines', // class name of 1px border (default: 'hairlines')
-        autoRem: true, // whether to transform to rem unit (default: false)
-      }),
+      'postcss-adaptive': config =>
+        require('postcss-adaptive')(config.mobileFlexible),
     },
   },
 };
