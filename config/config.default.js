9e680693e68e5786914d43b9d630661679ee23bd
okoala <dapixp@gmail.com>
2017-11-22 17:39:56 +0800

    feat: add postcss adapative

--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,15 @@
 exports.mobileFlexible = {};
+
+exports.styledJsx = {
+  postcss: {
+    plugins: {
+      'postcss-adaptive': require('postcss-adaptive')({
+        // baseDpr: 2,                 // base device pixel ratio (default: 2)
+        // remUnit: 75,                // rem unit value (default: 75)
+        // remPrecision: 6,            // rem value precision (default: 6)
+        // hairlineClass: 'hairlines', // class name of 1px border (default: 'hairlines')
+        autoRem: true, // whether to transform to rem unit (default: false)
+      }),
+    },
+  },
+};
